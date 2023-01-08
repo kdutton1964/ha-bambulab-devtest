c465b74e3ffeb6c79edfa4eebf8326329d1f2b1c
Greg Hesp <greg.hesp@gmail.com>
2023-01-08 21:21:02 +0000

    Updated coordinator.py

--- __init__.py
+++ __init__.py
@@ -14,6 +14,8 @@ PLATFORMS = (
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Bambu from a config entry."""
+
+    LOGGER.debug(f"Start async_setup_entry {hass}")
     coordinator = BambuCoordinator(hass, entry=entry)
     await coordinator.async_config_entry_first_refresh()
 
--- coordinator.py
+++ coordinator.py
@@ -1,10 +1,12 @@
 import asyncio
+from typing import Type
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, EVENT_HOMEASSISTANT_STOP
 from homeassistant.core import CALLBACK_TYPE, Event, HomeAssistant, callback
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+
 from .pybambu import Device as BambuDevice, BambuLab
 
 from .const import DOMAIN, LOGGER, SCAN_INTERVAL
@@ -18,32 +20,26 @@ class BambuCoordinator(DataUpdateCoordinator[BambuDevice]):
         self.bambu = BambuLab(
             entry.data[CONF_HOST]
         )
+        self.use_mqtt()
         self.unsub: CALLBACK_TYPE | None = None
         super().__init__(hass, LOGGER, name=DOMAIN, update_interval=SCAN_INTERVAL)
 
     @callback
     def use_mqtt(self) -> None:
-        def handle_callback(data):
-            LOGGER.debug(f"Callback data: ${data}")
-            self._async_update_data(data)
 
         async def listen() -> None:
             """Listen for state changes via WebSocket."""
             try:
                 LOGGER.debug("Connecting to Bambu")
                 await self.bambu.connect()
+                LOGGER.debug("Subscribing to Bambu")
+                await self.bambu.subscribe(callback=self.async_set_updated_data)
             except Exception as error:
-                LOGGER.error(f"error {error}")
+                LOGGER.error(f"Listen Error {error}")
                 if self.unsub:
                     self.unsub()
                     self.unsub = None
                 return
-
-            try:
-                LOGGER.debug("Initialising MQTT Subscription")
-                await self.bambu.subscribe(callback=handle_callback)
-            except Exception as errpr:
-                self.logger.error(f"error {errpr}")
 
             LOGGER.debug("Disconnecting from Bambu")
             await self.bambu.disconnect()
@@ -64,10 +60,12 @@ class BambuCoordinator(DataUpdateCoordinator[BambuDevice]):
 
         asyncio.create_task(listen())
 
-    async def _async_update_data(self) -> BambuDevice:
-        """Fetch data from Bambu."""
-
-        if self.unsub:
-            self.use_mqtt()
+    # async def _async_update_data(self):
+    #     """Manually fetches data.  Probably not needed"""
+    #
+    #     if not self.unsub:
+    #         self.use_mqtt()
+    #
+    #     return
 
 
