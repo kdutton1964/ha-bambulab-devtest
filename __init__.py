96c8570ec031c0bea1990ddd03283c3014de4211
Greg Hesp <768908+greghesp@users.noreply.github.com>
2023-01-11 17:30:31 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,48 +1,19 @@
-"""Unofficial BambuLab integration."""
+"""The Bambu Lab component."""
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
 from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN, LOGGER
-from .coordinator import BambuCoordinator
-
-PLATFORMS = (
-    Platform.SENSOR,
-)
+PLATFORMS = [Platform.SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up Bambu from a config entry."""
-
-    LOGGER.debug(f"Start async_setup_entry {hass}")
-    coordinator = BambuCoordinator(hass, entry=entry)
-    await coordinator.async_config_entry_first_refresh()
-
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    )
-
-    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
-
+    """Set up the Bambu Lab integration."""
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
-async def async_reload_entry(hass, entry):
-    await hass.config_entries.async_reload(entry.entry_id)
-
-
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        coordinator: BambuCoordinator = hass.data[DOMAIN][entry.entry_id]
-
-        # Ensure disconnected and cleanup stop sub
-        await coordinator.bambu.disconnect()
-        if coordinator.unsub:
-            coordinator.unsub()
-
-        del hass.data[DOMAIN][entry.entry_id]
-
-    return unload_ok
+    """Unload the Bambu Lab integration."""
+    # no data stored in hass.data
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
